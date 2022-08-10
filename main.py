9b73a8c214d28099fb1e8a19e762c32fde4204a0
Ymnenkaua(Tanya) <103893169+Ymnenkaua6556@users.noreply.github.com>
2022-08-10 20:42:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from kivy.app import App 
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.switch import Switch
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
@@ -13,21 +12,21 @@ yellow = (2.2,2.4,0.2,1)
 class Main(App):
     def build(self):
         main_layout = BoxLayout(orientation = "horizontal")
-        layout = BoxLayout(orientation = "vertical", spacing = 5, pos_hint = {"x" : 0.1, "y" : 0.49})
+        layout = BoxLayout(orientation = "vertical", spacing = 5, pos_hint = {"x" : 0.2, "y" : 0.6})
 
-        body1 = Image(source = "Body.png", pos_hint = {"center_y" : .5})
+        body1 = Image(source = "Body.png", pos_hint = {"center_y" : .5, "center_x" : .5, "x" : .9})
         main_layout.add_widget(body1)
 
-        btn_gender = Button(text = "Пол", size_hint = (0, 0), pos_hint = {"x" : .75},background_color=yellow,)
+        btn_gender = Button(text = "Пол", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75},background_color=yellow,)
         btn_gender.bind(on_press=self.on_press_gender)
         layout.add_widget(btn_gender)
         
 
-        btn_layer = Button(text = "Слой", size_hint = (0, 0), pos_hint = {"x" : .75},background_color=yellow)
+        btn_layer = Button(text = "Слой", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75},background_color=yellow)
         btn_layer.bind(on_press=self.on_press_layer)
         layout.add_widget(btn_layer)
 
-        btn_rotate = Button(text = "Поворот", size_hint = (0, 0), pos_hint = {"x" : .75},background_color=yellow)
+        btn_rotate = Button(text = "Поворот", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75},background_color=yellow)
         btn_rotate.bind(on_press=self.on_press_rotate)
         layout.add_widget(btn_rotate)
 
