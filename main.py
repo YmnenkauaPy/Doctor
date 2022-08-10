9292be176acc2b49e4358af9b3dae31f17317cf0
Ymnenkaua(Tanya) <103893169+Ymnenkaua6556@users.noreply.github.com>
2022-08-10 16:32:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from kivy.uix.label import Label
 from kivy.uix.switch import Switch
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.core.window import Window
 
+Window.clearcolor = (0, .8, .9, 1)    # установка цвета нового фона
+
+yellow = (2.2,2.4,0.2,1)
 
 class Main(App):
     def build(self):
@@ -14,17 +18,21 @@ class Main(App):
         body1 = Image(source = "Body.png", pos_hint = {"center_y" : .5})
         main_layout.add_widget(body1)
 
-        btn_gender = Button(text = "Пол", size_hint = (0, 0), size = (0.1, 0.1), pos_hint = {"x" : .9})
+        btn_gender = Button(text = "Пол", size_hint = (0.15, 0), size = (0.1, 0.1), pos_hint = {"x" : .82}, background_color = yellow)
         layout.add_widget(btn_gender)
+        btn_gender.bind(on_press=self.on_press_gender)
+        
 
-        btn_layer = Button(text = "Слой", size_hint = (0, 0), size = (0.1, 0.1), pos_hint = {"x" : .9})
+        btn_layer = Button(text = "Слой", size_hint = (0.15, 0), size = (0.1, 0.1), pos_hint = {"x" : .82}, background_color = yellow)
         layout.add_widget(btn_layer)
 
-        btn_rotate = Button(text = "Поворот", size_hint = (0, 0), size = (0.1, 0.1), pos_hint = {"x" : .9})
+        btn_rotate = Button(text = "Поворот", size_hint = (.2, 0), size = (0.5, 0.5), pos_hint = {"x" : .8}, background_color = yellow)
         layout.add_widget(btn_rotate)
 
         main_layout.add_widget(layout)
 
         return main_layout
+    def on_press_gender(self, isistance):
+        print("гендер сменен")
 
 Main().run()
