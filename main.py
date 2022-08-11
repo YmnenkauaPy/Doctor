8f259d3375f8ced641ffb9f759038acfd49b8b82
Ymnenkaua(Tanya) <103893169+Ymnenkaua6556@users.noreply.github.com>
2022-08-11 20:13:27 +0300

    gender

--- main.py
+++ main.py
@@ -9,12 +9,9 @@ from kivy.core.window import Window
 Window.clearcolor = (0, .8, .9, 1) #установка цвета нового фона
 yellow = (2.2,2.4,0.2,1)
 
-rotate = False
-
-class main(Screen):
+class man1(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        global rotate
         main_layout = BoxLayout(orientation = "horizontal")
         layout = BoxLayout(orientation = "vertical", spacing = 5, pos_hint = {"x" : 0.2, "y" : 0.6})
 
@@ -23,12 +20,12 @@ class main(Screen):
         main_layout.add_widget(body)
 
         btn_gender = Button(text = "Пол", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow,)
-        btn_gender.bind(on_press = self.on_press_gender)
+        btn_gender.on_press = self.on_press_gender
         layout.add_widget(btn_gender)
         
 
         btn_layer = Button(text = "Слой", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
-        btn_layer.bind(on_press = self.on_press_layer)
+        btn_layer.on_press = self.on_press_layer
         layout.add_widget(btn_layer)
 
         btn_rotate = Button(text = "Поворот", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
@@ -39,17 +36,17 @@ class main(Screen):
 
         self.add_widget(main_layout)
 
-    def on_press_gender(self, isistance):
-        print("гендер сменен")
+    def on_press_gender(self):
+        self.manager.current = 'woman1'
 
-    def on_press_layer(self, isistance):
+    def on_press_layer(self):
         print("Идет выбор слоя...")
     
     def rotate(self):
-        self.manager.current = 'rotate1'
+        self.manager.current = 'man2'
 
 
-class rotate1(Screen):
+class man2(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         main_layout = BoxLayout(orientation = "horizontal")
@@ -59,12 +56,12 @@ class rotate1(Screen):
         main_layout.add_widget(body)
 
         btn_gender = Button(text = "Пол", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow,)
-        btn_gender.bind(on_press = self.gender)
+        btn_gender.on_press = self.gender
         layout.add_widget(btn_gender)
         
 
         btn_layer = Button(text = "Слой", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
-        btn_layer.bind(on_press = self.layer)
+        btn_layer.on_press = self.layer
         layout.add_widget(btn_layer)
 
         btn_rotate = Button(text = "Поворот", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
@@ -75,20 +72,94 @@ class rotate1(Screen):
 
         self.add_widget(main_layout)
 
-    def gender(self, isistance):
-        print("гендер сменен")
+    def gender(self):
+        self.manager.current = 'woman1'
 
-    def layer(self, isistance):
+    def layer(self):
         print("Идет выбор слоя...")
     
     def rotate(self):
-        self.manager.current = 'main'
+        self.manager.current = 'man1'
+
+class woman1(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        main_layout = BoxLayout(orientation = "horizontal")
+        layout = BoxLayout(orientation = "vertical", spacing = 5, pos_hint = {"x" : 0.2, "y" : 0.6})
+
+        body = Image(source = "woman1.png", pos_hint = {"center_y" : .5, "center_x" : .5, "x" : .9})
+
+        main_layout.add_widget(body)
+
+        btn_gender = Button(text = "Пол", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow,)
+        btn_gender.on_press = self.on_press_gender
+        layout.add_widget(btn_gender)
+        
+
+        btn_layer = Button(text = "Слой", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
+        btn_layer.on_press = self.on_press_layer
+        layout.add_widget(btn_layer)
+
+        btn_rotate = Button(text = "Поворот", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
+        btn_rotate.on_press = self.rotate
+        layout.add_widget(btn_rotate)
+
+        main_layout.add_widget(layout)
+
+        self.add_widget(main_layout)
+
+    def on_press_gender(self):
+        self.manager.current = 'man1'
+
+    def on_press_layer(self):
+        print("Идет выбор слоя...")
+    
+    def rotate(self):
+        self.manager.current = 'woman2'
+
+class woman2(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        main_layout = BoxLayout(orientation = "horizontal")
+        layout = BoxLayout(orientation = "vertical", spacing = 5, pos_hint = {"x" : 0.2, "y" : 0.6})
+
+        body = Image(source = "woman2.png", pos_hint = {"center_y" : .5, "center_x" : .5, "x" : .9})
+
+        main_layout.add_widget(body)
+
+        btn_gender = Button(text = "Пол", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow,)
+        btn_gender.on_press = self.on_press_gender
+        layout.add_widget(btn_gender)
+        
+
+        btn_layer = Button(text = "Слой", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
+        btn_layer.on_press = self.on_press_layer
+        layout.add_widget(btn_layer)
+
+        btn_rotate = Button(text = "Поворот", size_hint = (None, None), size = (70, 70), pos_hint = {"x" : .75}, background_color = yellow)
+        btn_rotate.on_press = self.rotate
+        layout.add_widget(btn_rotate)
+
+        main_layout.add_widget(layout)
+
+        self.add_widget(main_layout)
+
+    def on_press_gender(self):
+        self.manager.current = 'man1'
+
+    def on_press_layer(self):
+        print("Идет выбор слоя...")
+    
+    def rotate(self):
+        self.manager.current = 'woman1'
 
 class Screen(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(main(name = 'main'))
-        sm.add_widget(rotate1(name = 'rotate1'))
+        sm.add_widget(man1(name = 'man1'))
+        sm.add_widget(man2(name = 'man2'))
+        sm.add_widget(woman1(name = 'woman1'))
+        sm.add_widget(woman2(name = 'woman2'))
         return sm
 
 app = Screen()
